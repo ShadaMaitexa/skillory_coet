19fa13667e3e3d0b9fce69376faf5aca0c5ce7ea
ShadaMaitexa <shada.maitexa@gmail.com>
2026-03-14 11:59:38 +0530

    feat: add email service configuration constants.

--- lib/utils/email_constants.dart
+++ lib/utils/email_constants.dart
@@ -1,5 +1,5 @@
 class EmailConstants {
-  static const String serviceId = 'YOUR_SERVICE_ID'; // Replace with EmailJS Service ID
-  static const String templateId = 'YOUR_TEMPLATE_ID'; // Replace with EmailJS Template ID
-  static const String publicKey = 'YOUR_PUBLIC_KEY'; // Replace with EmailJS Public Key
+  static const String serviceId = 'service_coqr5h5';
+  static const String templateId = 'template_bulniyr';
+  static const String publicKey = 'GYdbhUnkF0jmxJAc5';
 }
